636fe2a224495f13c106db0a6e849fd544ba1047
shuwei <swzhang296@gmail.com>
2016-11-12 20:54:41 +0100

    chagne body class

--- assets/js/myWeb/myWeb.js
+++ assets/js/myWeb/myWeb.js
@@ -20,7 +20,7 @@ app.config( function( $stateProvider, $urlRouterProvider ) {
 	        })
 
 		$stateProvider
-	  		.state('home', {
+	  		.state('aboutme', {
 	            url: '/aboutme',
 	            views:{
 	                'nav-top' : {
